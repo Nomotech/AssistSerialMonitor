7c90ae25f590f84a2d979fe25a054ef64a3ffcd7
nomotech <nmtts.1126@gmail.com>
2017-11-25 17:05:17 +0900

    CSS 適用

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function(){
   chrome.app.window.create('index.html', {
     'bounds': {
       'width': 800,
-      'height': 600, 
+      'height': 800, 
       'top': 0,
       'left': 100
     }
